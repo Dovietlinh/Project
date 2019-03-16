7badd6a24c74a11603bcb2f309d860641f359268
Dovietlinh <40727628+Dovietlinh@users.noreply.github.com>
2019-03-16 09:02:36 +0700

    Update MainActivity.java
    1

--- app/src/main/java/com/example/projectfood/MainActivity.java
+++ app/src/main/java/com/example/projectfood/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //đỗ viết linh
+        //IS1202
     }
 }
